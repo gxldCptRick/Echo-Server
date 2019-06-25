52437d57001f98f1ba5c15673599ca50ef91554c
ANDRES H CARRERA <andreshcar@live.com>
2019-06-25 10:25:12 -0600

    adding some comments so that we can focus on the protocol

--- src/main/java/com/gxldcptrick/echoserver/client/Main.java
+++ src/main/java/com/gxldcptrick/echoserver/client/Main.java
@@ -1,4 +1,16 @@
 package com.gxldcptrick.echoserver.client;
 
 public class Main {
+    /*
+    Once the connection between your server and your client is established the client should do the following:
+    1. Allow the user to type a single line string on the console.
+    2. Read in the string from the user and print it out preceded by the text “Client says:”
+    3. Send the string over the socket to the server.
+    4. Wait for a response from the server.
+    5. When the server responds print “Server says:” followed by the single line string received from the server.
+    Go to 1.
+    */
+    public static void main(String[] args) {
+
+    }
 }
--- src/main/java/com/gxldcptrick/echoserver/server/Main.java
+++ src/main/java/com/gxldcptrick/echoserver/server/Main.java
@@ -1,4 +1,16 @@
 package com.gxldcptrick.echoserver.server;
 
 public class Main {
+    /*
+    Once the connection is established the server does the following:
+    1. Wait for a message from the client.
+    2. Read in a single line string message from the client.
+    3. Print out “Received from client:” followed by the string from the client.
+    4. Print out “Sending to client:” followed by the string from the client.
+    5. Write the single line string received from the client back out to the client (echoing what the client said).
+    Go to 1.
+    */
+    public static void main(String[] args) {
+
+    }
 }
