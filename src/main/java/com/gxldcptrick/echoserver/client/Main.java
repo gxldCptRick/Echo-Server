e03a432bba183730b3ebc3bdcb9d6061fba64f74
ANDRES H CARRERA <andreshcar@live.com>
2019-06-26 08:20:42 -0600

    adding the changes that we made to the repo

--- src/main/java/com/gxldcptrick/echoserver/client/Main.java
+++ src/main/java/com/gxldcptrick/echoserver/client/Main.java
@@ -1,7 +1,14 @@
 package com.gxldcptrick.echoserver.client;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     /*
@@ -13,15 +20,55 @@ public class Main {
     5. When the server responds print “Server says:” followed by the single line string received from the server.
     Go to 1.
     */
-    private static int APP_PORT = 8080;
+    private static int APP_PORT;
     public static void main(String[] args) {
-        boolean isActive = true;
-        try(var clientSocket = new Socket("localhost", APP_PORT)){
+        var commandArgs = createArgumentsMap(args);
+        if(!commandArgs.containsKey("port")) throw new IllegalArgumentException("Port must be passed in as a command line argument.");
+        APP_PORT = Integer.parseInt(commandArgs.get("port"));
+        if(!commandArgs.containsKey("host")) throw new IllegalArgumentException("Host must be passed in as a command line argument.");
+        var host = commandArgs.get("host");
+        boolean isActive;
+        try(var clientSocket = new Socket(host, APP_PORT)){
+            System.out.println("..Connected to Server..");
             do{
-                // Do the server shit.
+               var input = getUserInput("Give Me a Message");
+                System.out.println("Client Says: " + input);
+                input += ".";
+               clientSocket.getOutputStream().write(input.getBytes());
+               var echoedMessage = readInputFromServer(clientSocket.getInputStream());
+                System.out.println("Server Says: " + echoedMessage);
+                isActive = getUserInput("Would you like to continue?(y/n)").equalsIgnoreCase("y");
             }while(isActive);
         }catch(IOException e){
-            e.printStackTrace();
+            if(e instanceof SocketException){
+                System.err.println("...Lost Connection To Host...");
+            }else{
+                e.printStackTrace();
+            }
         }
+    }
+
+    private static Map<String, String> createArgumentsMap(String[] args) {
+        var emptyMap = new HashMap<String, String>();
+        for (var arg: args) {
+            var argPair = arg.split("=");
+            emptyMap.put(argPair[0], argPair[1]);
+        }
+        return emptyMap;
+    }
+
+    private static String readInputFromServer(InputStream inputStream) throws IOException {
+        var serverMessage = new StringBuilder();
+        char currentCharacter;
+        while((currentCharacter = (char)inputStream.read()) != '.' && currentCharacter != 65535){
+            serverMessage.append(currentCharacter);
+        }
+        return serverMessage.toString();
+    }
+
+    public static String getUserInput(String message) throws IOException {
+        var buffy  = new BufferedReader( new InputStreamReader(System.in));
+        System.out.print(message + ": ");
+        return buffy.readLine().replaceAll("\\.", ";");
     }
 }
--- src/main/java/com/gxldcptrick/echoserver/server/Main.java
+++ src/main/java/com/gxldcptrick/echoserver/server/Main.java
@@ -1,7 +1,11 @@
 package com.gxldcptrick.echoserver.server;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
+import java.net.SocketException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     /*
@@ -13,17 +17,52 @@ public class Main {
     5. Write the single line string received from the client back out to the client (echoing what the client said).
     Go to 1.
     */
-    private static int APP_PORT = 8080;
+    private static int APP_PORT;
     public static void main(String[] args) {
+        var commandArgs = createArgumentsMap(args);
+        if(!commandArgs.containsKey("port")) throw new IllegalArgumentException("Port must be passed in as a command line parameter.");
+        APP_PORT = Integer.parseInt(commandArgs.get("port"));
         boolean isActive = true;
         try(var server = new ServerSocket(APP_PORT)) {
+            System.out.println("Listening on Port " + APP_PORT);
             try(var clientConnection = server.accept()){
+                System.out.println("Accepted Connection");
                 do {
-                    //Do server stuff
+                   var messageToEcho =  readMessageFromClient(clientConnection.getInputStream());
+                    if(!messageToEcho.isBlank()){
+                        System.out.println("Received from client: '" + messageToEcho + "'");
+                        System.out.println("Sending to client: '" + messageToEcho + "'");
+                        messageToEcho += ".";
+                        clientConnection.getOutputStream().write(messageToEcho.getBytes());
+                    }else{
+                        isActive = false;
+                    }
                 }while(isActive);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            if(e instanceof SocketException){
+                System.err.println("...Lost Connection To Client...");
+            }else{
+                e.printStackTrace();
+            }
         }
+    }
+
+    private static String readMessageFromClient(InputStream inputStream) throws IOException {
+        var clientMessage = new StringBuilder();
+        char currentCharacter;
+        while((currentCharacter = (char)inputStream.read()) != '.' && currentCharacter != 65535){
+            clientMessage.append(currentCharacter);
+        }
+        return clientMessage.toString();
+    }
+
+    private static Map<String, String> createArgumentsMap(String[] args) {
+        var emptyMap = new HashMap<String, String>();
+        for (var arg: args) {
+            var argPair = arg.split("=");
+            emptyMap.put(argPair[0], argPair[1]);
+        }
+        return emptyMap;
     }
 }
